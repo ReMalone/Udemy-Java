cc4022593e732f2c14f7cecfaf863c30ff0b2c43
Robert <darkgenezis@gmail.com>
2019-07-11 16:49:44 -0500

    Add files via upload

---
+++ Inherti/Project/src/app/Fish.java
@@ -0,0 +1,35 @@
+package app;
+
+public class Fish extends Animal{
+    
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int brain, int body, int size, int weight, int gills, int eyes, int fins){
+
+        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+
+    private void rest(){
+
+    }
+
+    private void moveMuscles(){
+
+    }
+
+    private void moveBackFin(){
+
+    }
+
+    private void swim(int speed){
+        moveMuscles();
+        moveBackFin();
+        super.move(speed);
+    }
+
+}
