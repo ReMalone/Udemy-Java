a681447b971a26f0eb8d3197d68314a6c1dd8623
Robert <darkgenezis@gmail.com>
2019-07-15 23:09:01 -0500

    Add files via upload

---
+++ ArrayChallenge/src/app/App.java
@@ -0,0 +1,63 @@
+package app;
+
+import java.util.Scanner;
+
+public class App {
+    private static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args)  {
+        
+        int[] myIntegers = getIntegers(5);
+        int[] sorted = sortIntegers(myIntegers);
+        printArray(sorted);
+
+    }
+
+    public static int[] getIntegers(int capacity){
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " integer values:\r");
+        for(int i=0; i<array.length; i++){
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array){
+        for(int i=0; i<array.length; i++){
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array){
+        int[] sortedArray = new int[array.length];
+        for(int i=0; i<array.length; i++){
+            sortedArray[i] = array[i];
+        }
+        //Optional method instead of for loop
+        //int[] sortedArray = Arrays.copyOf(array, array.length);
+
+
+        //While loop to see if sorted
+        boolean flag = true;
+        int temp;
+        while(flag){
+            flag = false;
+            //element 0   160
+            //element 1   50
+            //element 2   40
+            for(int i=0;i<sortedArray.length-1; i++){
+                if(sortedArray[i] < sortedArray[i+1]){
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+
+        return sortedArray;
+    }
+
+
+}
