4ca16d351cd8a34c232b08c121bf8532a1fb8d8e
Robert <darkgenezis@gmail.com>
2019-09-12 15:34:28 -0500

    Add files via upload

---
+++ GroceryListTest/GroceryList.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.util.ArrayList;
+
+
+public class GroceryList{
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String items){
+        groceryList.add(items);
+    }
+
+    public void printGroceryList(){
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for(int i=0; i<groceryList.size(); i++){
+            System.out.println((i+1) + ", " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(String currentItem, String newItem){
+        int position = findItem(newItem);
+        if(position >= 0){
+            modifyGroceryItem(position, newItem);
+        }
+    }
+
+    private void modifyGroceryItem(int position, String newItem){
+        groceryList.set(position, newItem);
+        System.out.println("Grocery Item " + (position+1) + " has been modified.");
+    }
+
+    private void removeGroceryItem(int position){
+        groceryList.remove(position);
+    }
+
+    public void removeGroceryItem(String item){
+        int position = findItem(item);
+        if(position >= 0){
+            removeGroceryItem(position);
+        }
+    }
+
+    private int findItem(String searchItem){
+        return groceryList.indexOf(searchItem);
+    }
+
+    public boolean onFile(String searchItem){
+        int position = findItem(searchItem);
+        if(position >=0){
+            return true;
+        }
+        return false;
+    }
+
+
+}
